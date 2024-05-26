22facf0df50bdc019308971cdc04660fcb89a66a
ravipandey1298 <68158316+ravipandey1298@users.noreply.github.com>
2024-05-26 11:59:59 +0000

    CoversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true;
+
+console.log(typeof score);
+console.log(typeof(score))
+
+let sharePrice = 33.5;
+console.log(typeof sharePrice)
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "7";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false;
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
